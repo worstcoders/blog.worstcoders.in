d2b4fc9626fb8cc1fcd62d0a76d376ece519d0c2
raj patel <kakhilesh79@gmail.com>
2021-01-17 17:32:14 +0530

    yathartha

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with swag by raj
+          Learn React with swag by raj patel
         </a>
       </header>
     </div>
